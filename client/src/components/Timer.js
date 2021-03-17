7abaf56f94f4c013d4d9e2e8b6aef24521353920
Verena Isele <v.j.isele@gmail.com>
2021-03-17 17:12:30 +0100

    countdown verkürzt und code aufgeräumt

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,22 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-/* function Timer() {
-  // Declare a new state variable, which we'll call "count"
-
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-      </div>
-  );
-}
-export default Timer; */
-
 export default function Timer({ seconds }) {
   const [timeLeft, setTimeLeft] = useState(seconds);
 
-  //ausgelagerte Funktion für Countdown
+  //Funktion für Countdown
   const startCountdown = () => {
     setTimeLeft(timeLeft - 1);
   };
@@ -31,36 +18,8 @@ export default function Timer({ seconds }) {
 
   return (
     <div>
+      <button onClick={() => setTimeLeft(10)}>Start Countdown</button>
       <h1>{timeLeft}</h1>
-      <button onClick={() => setTimeLeft(30)}>Start Countdown</button>
     </div>
-    /*Funktion muss in OHM-Komponente übergeben werden..*/
   );
 }
-
-/* const Timer = ({ seconds }) => {
-  // initialize timeLeft with the seconds prop
-  const [timeLeft, setTimeLeft] = useState(seconds);
-
-  useEffect(() => {
-    // exit early when we reach 0
-    if (!timeLeft) return;
-
-    // save intervalId to clear the interval when the
-    // component re-renders
-    const intervalId = setInterval(() => {
-      setTimeLeft(timeLeft - 1);
-    }, 1000);
-
-    // clear interval on re-render to avoid memory leaks
-    return () => clearInterval(intervalId);
-    // add timeLeft as a dependency to re-rerun the effect
-    // when we update it
-  }, [timeLeft]);
-
-  return (
-    <div>
-      <h1>{timeLeft}</h1>
-    </div>
-  );
-}; */
--- client/src/pages/Ohm.js
+++ client/src/pages/Ohm.js
@@ -5,10 +5,10 @@ export default function Ohm() {
   return (
     <div>
       <Heading>Willkommen</Heading>
-      <p>Nimm dir 30 Sekunden Zeit.</p>
+      <p>Nimm dir 10 Sekunden Zeit.</p>
       <p>Frag dich: Wie geht es mir?</p>
       <p>Bereit?</p>
-      <Timer seconds={0} />
+      <Timer seconds={''} />
     </div>
   );
 }
